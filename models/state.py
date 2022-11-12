43cdf6def3a8355e00c6e68a985919588fe01943
BirukG <byafet3@gmail.com>
2022-11-12 06:03:37 -0800

    fixed

--- models/state.py
+++ models/state.py
@@ -3,7 +3,6 @@
 
 
 from models.base_model import BaseModel, Base
-from models import storage_type
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.engine.file_storage import FileStorage
@@ -13,23 +12,17 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class definition in the next line"""
     __tablename__ = 'states'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade="all, delete")
-    else:
-        name = ''
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade="all, delete")
 
-
-    @property
-    def cities(self):
-        '''returns the list of City instances with state_id
-                equals the current State.id
-                FileStorage relationship between State and City
-        '''
-        from models import storage
-        related_cities = []
-        cities = storage.all(City)
-        for city in cities.values():
-            if city.state_id == self.id:
-                related_cities.append(city)
-        return related_cities
+    if getenv("HBNB_TYPE_STORAGE") != 'db':
+        @property
+        def cities(self):
+            from models import storage
+            from models.city import City
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return
