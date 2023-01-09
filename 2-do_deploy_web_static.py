416a31a5d9299ad564bc0f76333f71f52a4f1f6a
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-01-09 18:31:29 +0000

    applied a fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """ This python script distributes an archive to web servers,
 using the function do_deploy"""
+import logging
 from os.path import exists
 from fabric.api import run, put, env
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 env.hosts = ["34.229.49.169", "54.162.223.76"]
 env.key_filename = '~/.ssh/id_rsa'
@@ -10,40 +14,32 @@ env.key_filename = '~/.ssh/id_rsa'
 def do_deploy(archive_path):
     """Deployes the archive to the webserver"""
     if not exists(archive_path):
+        logging.error("Archive file does not exist")
         return False
 
     full_name = archive_path.split("/")[1]
     file_name = archive_path.split("/")[1].split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(
-           full_name)).failed is True:
+    # Transfer the archive file to the server
+    try:
+        put(archive_path, "/tmp/{}".format(full_name))
+    except Exception as e:
+        logging.error("Failed to transfer archive file: %s", e)
         return False
 
-    if run("rm -rf /data/web_static/releases/{}/".format(
-           file_name)).succeeded is False:
+    # Remove the existing directory
+    if run("rm -rf /data/web_static/releases/{}/".format(file_name)).failed:
+        logging.error("Failed to remove existing directory")
         return False
 
-    if run("mkdir -p /data/web_static/releases/{}/".format(
-           file_name)).succeeded is False:
+    # Create a new directory
+    if run("mkdir -p /data/web_static/releases/{}/".format(file_name)).failed:
+        logging.error("Failed to create new directory")
         return False
 
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-           full_name, file_name)).succeeded is False:
+    # Extract the archive file into the new directory
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(full_name, file_name)).failed:
+        logging.error("Failed to extract archive file")
         return False
 
-    if run("rm /tmp/{}".format(full_name)).failed is True:
-        return False
-
-    if run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(file_name, file_name)).failed is True:
-        return False
-
-    if run("rm -rf /data/web_static/releases/{}/web_static".format(file_name)).failed is True:
-        return False
-
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(file_name)).succeeded is False:
-        return False
-
-    return True
+    logging.info("Deployment successful")
