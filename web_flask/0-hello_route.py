7a86d2b5d75adc2bd177c780c276cf32a3cb0edf
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-01-11 13:08:11 +0000

    uptimize wsgi file for production

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,10 +9,12 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def idex():
+def index():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route('/airbnb-onepage/')
+def airbnb_onepage():
+    return index()
+
